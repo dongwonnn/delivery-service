52cb5b86c54c96c648305025ad43dc722bed9a88
dongwon <kkum9408@gmail.com>
2021-03-23 21:16:44 +0900

    [#27] Update input func

--- src/components/DetailMenuModal.js
+++ src/components/DetailMenuModal.js
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMenus } from '../modules/data';
 import './DetailMenuModal.scss';
-import SelectOptions from './SelectOptions';
 
 const transStrToInt = (strPrice) => Number(strPrice.replace(',', ''));
 
@@ -14,14 +13,11 @@ const DetailMenuModal = ({ menuId }) => {
   }, [menuId, dispatch]);
 
   const [menuCnt, setMenuCnt] = useState(1);
-
+  const [sumMenuPrice, setSumMenuPrice] = useState(0);
   const [defaultPrice, setDefaultPrice] = useState(undefined);
   const [totalPrice, setTotalPrice] = useState(undefined);
-  useEffect(() => {
-    const tempPrice = detailMenus ? transStrToInt(detailMenus.price) : 0;
-    setDefaultPrice(tempPrice);
-    setTotalPrice(tempPrice);
-  }, [detailMenus]);
+
+  const formRef = useRef(null);
 
   const onMinusBtn = () => {
     if (menuCnt > 1) {
@@ -38,18 +34,40 @@ const DetailMenuModal = ({ menuId }) => {
   };
 
   useEffect(() => {
-    const nextTotalPrice = defaultPrice * menuCnt;
-    setTotalPrice(nextTotalPrice);
-  }, [menuCnt, defaultPrice]);
+    const tempPrice = detailMenus
+      ? transStrToInt(detailMenus.price)
+      : undefined;
+    setDefaultPrice(tempPrice);
+    setTotalPrice(tempPrice);
+  }, [detailMenus]);
 
-  const onInputBtn = (menuPrice) => {
-    const nextDefaultPrice = defaultPrice + menuPrice;
-    setDefaultPrice(nextDefaultPrice);
+  const onInputBtn = (min, check) => {
+    const formData = new FormData(formRef.current);
+
+    let menuPrice = 0;
+    let size = 0;
+
+    for (var value of formData.values()) {
+      menuPrice += transStrToInt(value);
+      size++;
+    }
+
+    setSumMenuPrice(menuPrice);
+  };
+
+  useEffect(() => {
+    const nextTotalPrice = defaultPrice * menuCnt + sumMenuPrice * menuCnt;
+    setTotalPrice(nextTotalPrice);
+  }, [menuCnt, defaultPrice, sumMenuPrice]);
+
+  const onSubmitBtn = () => {
+    const formData = new FormData(formRef.current);
   };
 
   if (detailMenus === null) {
     return <div>로딩 중</div>;
   }
+  detailMenus.option_groups.sort((a, b) => b.required - a.required);
 
   return (
     <div className="detail-menu-modal">
@@ -60,7 +78,6 @@ const DetailMenuModal = ({ menuId }) => {
         <h2>{detailMenus.name}</h2>
         <p>{detailMenus.description}</p>
       </div>
-
       <div className="detail-modal-price">
         <p>가격</p>
         <p>{totalPrice}원</p>
@@ -74,28 +91,34 @@ const DetailMenuModal = ({ menuId }) => {
         </div>
       </div>
       <div className="detail-modal-select-menus">
-        <form>
+        <form ref={formRef}>
           {detailMenus.option_groups.map((group) => (
             <div className="menus-group" key={group.name}>
-              <div className="menus-group-name">{group.name}</div>
+              <div className="menus-group-name">
+                <p>{`${group.name} (${group.min} ~ ${group.max})`}</p>
+                <p>{group.required ? '필수 선택' : ' '}</p>
+              </div>
               {group.options.map((option) => (
                 <div className="menus-group-menu" key={option.id}>
                   <div>
                     <input
                       type="checkbox"
                       id={option.id}
-                      name={group.name}
-                      value={option.name}
-                      onClick={() => onInputBtn(transStrToInt(option.price))}
+                      name={option.name}
+                      value={transStrToInt(option.price)}
+                      onClick={() => onInputBtn(group.min, group.required)}
                     />
                     <label htmlFor={option.id}>{option.name}</label>
                   </div>
-                  <p>+ {option.price}</p>
+                  <p>+ {option.price}원</p>
                 </div>
               ))}
             </div>
           ))}
         </form>
+      </div>
+      <div className="detail-model-submit">
+        <button onClick={() => onSubmitBtn()}>주문표에 추가</button>
       </div>
     </div>
   );
